01ea734671d1a62947a47feb1aac0ac14214018b
hiknot <hiroktdev@gmail.com>
2019-02-27 21:12:05 +0900

    create lstm

--- DQNBot_LSTM.py
+++ DQNBot_LSTM.py
@@ -1,9 +1,10 @@
+import sys
 from time import sleep
 import gym
 import gym.spaces
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
+from keras.layers import Dense, Activation, Flatten, LSTM
 from keras.optimizers import Adam
 
 from rl.agents.dqn import DQNAgent
@@ -29,16 +30,16 @@ class DQNBot(gym.core.Env):
         self.STAY = 2
         self.action_space = gym.spaces.Discrete(2)
 
-        self.con = sqlite3.connect('board_2.db')
+        self.con = sqlite3.connect('board_1.db')
         self.cur = self.con.cursor()
         
-        df_low = psql.read_sql('SELECT 0, min(ask_price_100), min(ask_price_200), min(ask_price_300), min(ask_price_500), min(ask_price_800), min(ask_price_1300), min(ask_price_2100), min(ask_price_3400), min(ask_price_5500), min(ask_price_8900), \
+        df_low = psql.read_sql('SELECT 0, min(mid_price), min(ask_price_100), min(ask_price_200), min(ask_price_300), min(ask_price_500), min(ask_price_800), min(ask_price_1300), min(ask_price_2100), min(ask_price_3400), min(ask_price_5500), min(ask_price_8900), \
                     min(bid_price_100), min(bid_price_200), min(bid_price_300), min(bid_price_500), min(bid_price_800), min(bid_price_1300), min(bid_price_2100), min(bid_price_3400), min(bid_price_5500), min(bid_price_8900) FROM boards;', self.con) # DBからPandasデータフレーム取得
 
-        df_high = psql.read_sql('SELECT 1, max(ask_price_100), max(ask_price_200), max(ask_price_300), max(ask_price_500), max(ask_price_800), max(ask_price_1300), max(ask_price_2100), max(ask_price_3400), max(ask_price_5500), max(ask_price_8900), \
+        df_high = psql.read_sql('SELECT 1, max(mid_price), max(ask_price_100), max(ask_price_200), max(ask_price_300), max(ask_price_500), max(ask_price_800), max(ask_price_1300), max(ask_price_2100), max(ask_price_3400), max(ask_price_5500), max(ask_price_8900), \
                     max(bid_price_100), max(bid_price_200), max(bid_price_300), max(bid_price_500), max(bid_price_800), max(bid_price_1300), max(bid_price_2100), max(bid_price_3400), max(bid_price_5500), max(bid_price_8900) FROM boards;', self.con) # DBからPandasデータフレーム取得
         
-        board_df = psql.read_sql('SELECT ask_price_100, ask_price_200, ask_price_300, ask_price_500, ask_price_800, ask_price_1300, ask_price_2100, ask_price_3400, ask_price_5500, ask_price_8900, \
+        board_df = psql.read_sql('SELECT mid_price, ask_price_100, ask_price_200, ask_price_300, ask_price_500, ask_price_800, ask_price_1300, ask_price_2100, ask_price_3400, ask_price_5500, ask_price_8900, \
                                         bid_price_100, bid_price_200, bid_price_300, bid_price_500, bid_price_800, bid_price_1300, bid_price_2100, bid_price_3400, bid_price_5500, bid_price_8900 FROM boards;', self.con) # DBからPandasデータフレーム取得
         
         board_mid_df = psql.read_sql('SELECT mid_price FROM boards;', self.con) # DBからPandasデータフレーム取得
@@ -73,15 +74,6 @@ class DQNBot(gym.core.Env):
             elif self.pos[0] == self.SELL:
                 reward = self.pos[1] - self.get_midprice(self.step_count)
                 self.pos = [self.STAY, 0]
-                self.hold_step = 0
-            '''
-            elif self.pos[0] == self.BUY:
-                self.hold_step += 1
-                if 10 < self.hold_step:
-                    reward = self.get_midprice(self.step_count) - self.pos[1]
-                    self.pos = [self.STAY, 0]
-                    self.hold_step = 0
-            '''
             
         elif action == self.SELL:
             if self.pos[0] == self.STAY:
@@ -89,16 +81,7 @@ class DQNBot(gym.core.Env):
             elif self.pos[0] == self.BUY:
                 reward = self.get_midprice(self.step_count) - self.pos[1]
                 self.pos = [self.STAY, 0]
-                self.hold_step = 0
-            '''
-            elif self.pos[0] == self.SELL:
-                self.hold_step += 1
-                if 10 < self.hold_step:
-                    reward = self.pos[1] - self.get_midprice(self.step_count)
-                    self.pos = [self.STAY, 0]
-                    self.hold_step = 0
-            '''
-        
+                    
         # 次のstate、reward、終了したかどうか、追加情報の順に返す
         # 追加情報は特にないので空dict
         return np.insert(self.get_state(self.step_count), 0, self.pos[0]), reward, done, {}
@@ -108,7 +91,6 @@ class DQNBot(gym.core.Env):
         self.pos = [self.STAY, 0]
         self.profit = 0
         self.step_count = 0
-        self.hold_step = 0
         return np.insert(self.get_state(self.step_count), 0, self.pos[0])
 
 class EpisodeLogger(rl.callbacks.Callback):
@@ -131,23 +113,14 @@ class EpisodeLogger(rl.callbacks.Callback):
 if __name__ == "__main__":
     env = DQNBot()
     nb_actions = env.action_space.n
+    
+    input_shape = (1,) + env.observation_space.shape
 
     # DQNのネットワーク定義
     model = Sequential()
-    model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-    
-    # model.add(Dense(50, activation='relu', kernel_initializer=TruncatedNormal(stddev=0.0001), bias_initializer='ones'))
-    # model.add(Dense(25, activation='relu', kernel_initializer=TruncatedNormal(stddev=0.0001), bias_initializer='ones'))
-
-    model.add(Dense(16))
-    model.add(Activation('relu'))
-    model.add(Dense(16))
-    model.add(Activation('relu'))
-    model.add(Dense(1))
-    model.add(Activation('relu'))
-    
-    model.add(Dense(nb_actions))
-    model.add(Activation('linear'))
+    model.add(LSTM(units=12, return_sequences=False, input_shape=input_shape))
+    # model.add(LSTM(units=2, return_sequences=False))
+    model.add(Dense(units=nb_actions, activation='linear'))
     print(model.summary())
 
     # experience replay用のmemory
@@ -155,14 +128,19 @@ if __name__ == "__main__":
     # 行動方策はオーソドックスなepsilon-greedy。ほかに、各行動のQ値によって確率を決定するBoltzmannQPolicyが利用可能
     # policy = GreedyQPolicy()
     # policy = BoltzmannQPolicy()
-    policy = EpsGreedyQPolicy(eps=0.4)
+    policy = EpsGreedyQPolicy(eps=0.1)
     dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=100,
                 target_model_update=1e-2, policy=policy)
     dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
-    history = dqn.fit(env, nb_steps=8000, visualize=False, verbose=2, nb_max_episode_steps=100)
-    #学習の様子を描画したいときは、Envに_render()を実装して、visualize=True にします,
-
+    if sys.argv[1] == 'train':
+        #学習の様子を描画したいときは、Envに_render()を実装して、visualize=True にします,
+        history = dqn.fit(env, nb_steps=3600, visualize=False, verbose=2, nb_max_episode_steps=1200)
+        dqn.save_weights('dqnbot.h5')
+    
+    elif sys.argv[1] == 'test':
+        dqn.load_weights('dqnbot.h5')
+    
     cb_ep = EpisodeLogger()
     dqn.test(env, nb_episodes=1, visualize=False, callbacks=[cb_ep])
 
@@ -176,18 +154,6 @@ if __name__ == "__main__":
             ac_list.append(-1)
 
     '''
-    count = 0
-    rw = 0
-    rw_list = []
-    for ep_rw in list(cb_ep.rewards.values())[0]:
-        if ac_list[count] != -1:
-            rw = rw + ep_rw
-        rw_list.append(rw)
-        count += 1
-    plt.plot(rw_list)
-    print("PL: " + str(rw))
-    '''
-
     rw = 0
     rw_list = []
     for ep_rw in list(cb_ep.rewards.values())[0]:
@@ -211,10 +177,11 @@ if __name__ == "__main__":
         count += 1
     print("BUY : " + str(ac_list.count(0)))
     print("SELL: " + str(ac_list.count(1)))
+    '''
     
-    plt.xlabel("step")
-    plt.ylabel("rw")
-    plt.show()
+    # plt.xlabel("step")
+    # plt.ylabel("rw")
+    # plt.show()
     
     # plt.plot(env.get_midprice_list())
     # plt.show()
